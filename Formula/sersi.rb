593736ec63eb8d2bbd06249e2c2f78714db1b9b8
KristienN <knyamutsaka@outlook.com>
2025-06-07 17:02:44 +0100

    update formula

--- Formula/sersi.rb
+++ Formula/sersi.rb
@@ -5,10 +5,10 @@ class Sersi < Formula
 
     if Hardware::CPU.intel?
       url "https://github.com/sersi-project/sersi/releases/download/v0.0.0-alpha/sersi-darwin-amd64.tar.gz"
-      sha256 "sha256:105ff6cc8cf51670e45559aeedf5a5bf256cb39415baa35e246af8229b0eb3e8"
+      sha256 "105ff6cc8cf51670e45559aeedf5a5bf256cb39415baa35e246af8229b0eb3e8"
     elsif Hardware::CPU.arm?
       url "https://github.com/sersi-project/sersi/releases/download/v0.0.0-alpha/sersi-darwin-arm64.tar.gz"
-      sha256 "sha256:46a72d3017743b3ad6a691c2222f5afae540cc1501cab76c7e1aa029be3eb14d"
+      sha256 "46a72d3017743b3ad6a691c2222f5afae540cc1501cab76c7e1aa029be3eb14d"
     end
   
     def install
